30f6d04f61d3a6b36d7fa060f2dc6076285ab302
Aapo Humina <rageidos@gmail.com>
2023-04-19 08:26:53 +0300

    added files to github

--- python/python/sentiment_analysis.py
+++ python/python/sentiment_analysis.py
@@ -2,8 +2,6 @@ from random import shuffle
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from statistics import mean
 
-import numpy as np
-import pandas as pd
 import nltk
 
 nltk.download([
@@ -27,19 +25,15 @@ def isPostive_Negative_or_Neutral(text: str) -> str:
         return str(sentiment)+" Neutral"
 
 
-example_input1 = "Jere is great teacher but school is hectic and there is not always time to do all homework"
-example_input2 = "Jere is THE BEST! teacher ever and I want to learn everything from him!!"
-example_input3 = "Jere is an asshole and no one likes him!!"
-example_input4 = "Jere is a fucking IDIOT and asshole and no one likes him!!"
+example_input1 = "Matti Meikäläinen is great teacher but school is hectic and there is not always time to do all homework"
+example_input2 = "Matti Meikäläinen is THE BEST! teacher ever and I want to learn everything from him!!"
+example_input3 = "Matti Meikäläinen is an asshole and no one likes him!!"
+example_input4 = "Matti Meikäläinen is a fucking IDIOT and asshole and no one likes him!!"
 
 stopwords = nltk.corpus.stopwords.words("english")
 
 sia = SentimentIntensityAnalyzer()
 
-df = pd.read_csv(
-    "C:/Users/Aapo/py-sentiment/python/TextFiles/reviews.csv", sep="\t")
-
-print(df.head(99))
 print("")
 print(isPostive_Negative_or_Neutral(example_input1))
 print(isPostive_Negative_or_Neutral(example_input2))
